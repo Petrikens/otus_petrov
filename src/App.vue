5e2412923f420368840f1dabbee6a7df86155644
Anton Petrov <anton@900.direct>
2025-05-09 14:16:48 +0300

    create vite vue 3 project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Otus Courses First Homework" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
