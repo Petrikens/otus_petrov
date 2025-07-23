8e90a0c8017418dd7ba18f0d5b8af1d27eea570e
Anton Petrov <anton@900.direct>
2025-07-23 12:07:20 +0300

    websocket fixes

--- src/components/AppBitcoinTicker.vue
+++ src/components/AppBitcoinTicker.vue
@@ -11,9 +11,13 @@ import { ref, onMounted, onBeforeUnmount } from 'vue'
 
 const price = ref(null)
 let socket = null
+const isConnected = ref(false)
 
 onMounted(() => {
   socket = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade')
+  socket.onopen = () => {
+    isConnected.value = true
+  }
 
   socket.onmessage = (event) => {
     const data = JSON.parse(event.data)
@@ -22,6 +26,10 @@ onMounted(() => {
 
   socket.onerror = (err) => {
     console.error('WebSocket error:', err)
+    isConnected.value = false
+  }
+  socket.onclose = () => {
+    isConnected.value = false
   }
 })
 
